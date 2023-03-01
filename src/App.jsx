7f98e41fb26a2f295ad0252c80ad11c833b751c8
vmonney <45258650+vmonney@users.noreply.github.com>
2023-03-01 11:32:19 +0100

    Configuring React and ESLint

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom';
+import Pet from './Pet';
+
+const App = () => {
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="dog" breed="Havanese" />
+    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+    <Pet name="Doink" animal="cat" breed="Mixed" />
+  </div>;
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
